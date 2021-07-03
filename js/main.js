4a3694b57b5fa0da77933ec269dab185e195c682
Edgar Bryan Lopez M <edgar360456@gmail.com>
2021-07-03 13:01:37 -0500

    Cambios en pagina principal

---
+++ js/main.js
@@ -0,0 +1,47 @@
+window.sr=ScrollReveal();
+
+
+sr.reveal('.logos',{
+   duration:2000,
+    origin:'letf',
+    distance: '-100px'
+});
+sr.reveal('.menus',{
+	duration:2000,
+	 origin:'bottom',
+	 distance: '100px'
+     });
+     sr.reveal('.xd',{
+	duration:2000,
+	 origin:'bottom',
+	 distance: '100px'
+     });
+     
+     sr.reveal('.texts',{
+	duration:2000,
+	 origin:'bottom',
+	 distance: '100px'
+     });
+
+     sr.reveal('.separador1',{
+	duration:2000,
+	 origin:'left',
+	 distance: '100px'
+     });
+
+     sr.reveal('.separador2',{
+	duration:2000,
+	 origin:'right',
+	 distance: '100px'
+     });
+     sr.reveal('.vs',{
+	duration:2000,
+	 origin:'left',
+	 distance: '100px'
+     });
+
+     sr.reveal('.mapas',{
+	duration:2000,
+	 origin:'bottom',
+	 distance: '100px'
+     });
---
+++ js/requisitos.js
@@ -0,0 +1,60 @@
+window.sr=ScrollReveal();
+
+
+sr.reveal('.c1',{
+   duration:1000,
+    origin:'letf',
+    distance: '-100px'
+});
+
+sr.reveal('.mapa-g',{
+	duration:2500,
+	 origin:'letf',
+	 distance: '-100px'
+     });
+
+sr.reveal('.c2',{
+	duration:1500,
+	 origin:'letf',
+	 distance: '-100px'
+     });
+
+     sr.reveal('.izq-t',{
+	duration:4000,
+	 origin:'letf',
+	 distance: '-300px'
+     });
+     sr.reveal('.der-t',{
+	duration:4000,
+	 origin:'right',
+	 distance: '300px'
+     });
+
+
+     sr.reveal('.c2',{
+	duration:1500,
+	 origin:'letf',
+	 distance: '-100px'
+     });
+
+
+     sr.reveal('.c3',{
+	duration:2000,
+	 origin:'letf',
+	 distance: '-100px'
+     });
+     sr.reveal('.c4',{
+	duration:2500,
+	 origin:'letf',
+	 distance: '-100px'
+     });
+     sr.reveal('.c5',{
+	duration:3000,
+	 origin:'letf',
+	 distance: '-100px'
+     });
+     sr.reveal('.c6',{
+	duration:3500,
+	 origin:'letf',
+	 distance: '-100px'
+     });
